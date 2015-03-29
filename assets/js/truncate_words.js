da27fdf6c3e130f3355e789256dec7d63454c3bb
Xi Chen <xi.chen@rea-group.com>
2015-03-29 21:48:56 +0800

    Truncate words for posts in homepage.

---
+++ assets/js/truncate_words.js
@@ -0,0 +1,32 @@
+$(function() {
+    function traverse($node, len, maxCount) {
+        var reachMaxCount = len > maxCount;
+        if (reachMaxCount) {
+            $node.hide();
+        }
+        var $contents = $node.contents();
+        for (var i = 0; i < $contents.length; ++i) {
+            if (reachMaxCount) {
+                $contents.eq(i).hide();
+                continue;
+            }
+            if ($contents[i].nodeType == 3) { // TextNode
+                var tmp = len;
+                var s = $contents[i].nodeValue;
+                len += s.length;
+                reachMaxCount = len > maxCount;
+                if (reachMaxCount) {
+                    $contents[i].nodeValue = s.substring(0, maxCount - tmp);
+                }
+            }
+            else if ($contents[i].nodeType == 1) { // Element
+                len = traverse($contents.eq(i), len, maxCount);
+            }
+        }
+        return len;
+    }
+    $('.post-excerpt').each(function() {
+        traverse($(this), 0, 200);
+        $(this).after('...');
+    });
+});
